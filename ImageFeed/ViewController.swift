84acfdc24b24e7f090ce72377a790565bf15fc1c
Aleksandra_Gaykova <sara27.01.2002@gmail.com>
2025-03-22 12:27:04 +0300

    Initial Commit

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Александра Гайкова on 22.03.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
