df95511bb3fe1a808ac7cfc1b56bc8e91fb252ca
Александра Гайкова <sara27.01.2002@gmail.com>
2025-03-25 17:55:18 +0300

    добавлена картинка, дата и кнопка лайка

---
+++ ImageFeed/ImageList/ImageListCell.swift
@@ -0,0 +1,11 @@
+//
+//  ImageListCell.swift
+//  ImageFeed
+//
+//  Created by Александра Гайкова on 23.03.25.
+//
+import UIKit
+
+final class ImageListCell: UITableViewCell {
+    static let reuseIdentifier = "ImageListCell"
+}
